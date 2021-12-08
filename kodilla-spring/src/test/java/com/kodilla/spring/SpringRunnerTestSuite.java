a9545658ec30631e143b8d718b3d7bc3e93616a6
Pawel Demichowicz <pawel.demichowicz@interia.pl>
2021-12-08 19:54:07 +0100

    Introducing to Spring

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.spring;
+
+import com.kodilla.spring.shape.Circle;
+import com.kodilla.spring.shape.Shape;
+import com.kodilla.spring.shape.Triangle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class SpringRunnerTestSuite {
+
+    @Test
+    public void testCircleLoadedIntoContainer() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Circle.class);
+        //When
+        String name = shape.getShapeName();
+        //Then
+        assertEquals("This is a circle.", name);
+    }
+
+    @Test
+    public void testTriangleLoadedIntoContainer() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Triangle.class);
+        //When
+        String name = shape.getShapeName();
+        //Then
+        assertEquals("This is a triangle.", name);
+    }
+}
